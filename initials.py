f022ea22ed5c19802bf6c705261d2ca67ddb2b2e
HAFSA AHMED HAMA - 2425402422 <2425402422@live.gctu.edu.gh>
2026-05-31 10:39:50 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,6 +1,32 @@
-
+# File: initials.py
+# Description: Printing out my initials per specific parameters
+# Assignment Number: 2
+# Name: Hafsa Ahmed Hama
+# STUDENT ID: 2425402422
+# Email: 2425402422@live.gctu.edu.gh
+# Grader: Augustus Buckman
+# On my honor, Hafsa Ahmed Hama, this programming assignment is my own work
+# and I have not provided this code to any other student.
 def main():
   pass #code goes here
+  print()
+
+   print("...HAH")
+
+   print()
+
+   print("...HHHH.......HHHH........AAAAAAAAAAAA........HHHH.......HHHH.....")
+   print("...HHHH.......HHHH........AAAAAAAAAAAA........HHHH.......HHHH.....")
+   print("...HHHH.......HHHH........AAAA....AAAA........HHHH.......HHHH.....")
+   print("...HHHH.......HHHH........AAAA....AAAA........HHHH.......HHHH.....")
+   print("...HHHH.HHHH..HHHH........AAAA.AA.AAAA........HHHH.HHHH..HHHH.....")
+   print("...HHHH.HHHH..HHHH........AAAA.AA.AAAA........HHHH.HHHH..HHHH.....")
+   print("...HHHH.......HHHH........AAAA....AAAA........HHHH.......HHHH.....")
+   print("...HHHH.......HHHH........AAAA....AAAA........HHHH.......HHHH.....")
+   print("...HHHH.......HHHH...**...AAAA....AAAA...**...HHHH.......HHHH...**")
+   print("...HHHH.......HHHH...**...AAAA....AAAA...**...HHHH.......HHHH...**")
+ 
+   print()
 
 
 
